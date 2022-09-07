70a9987d32134e1736ff7e9cfc26f11ebead49a0
Norberto Ruiz <narj@norbertoruiz.com>
2022-09-07 00:46:11 -0500

    Recorrer string con for

--- 5-blucles/for.py
+++ 5-blucles/for.py
@@ -1,11 +1,8 @@
 # a = list(range(1000))
 # print(a)
 
-for contador in range (1, 1001):#valor final llega a una posicion anterior
+for contador in range(1, 1001):#valor final llega a una posicion anterior
     print(contador)
 
 for i in range(10):
     print(11 * i)
-
-# if __name__ == '__main__':
-#     pass
