eb00a3aec2de211c529deced923e3eed772f6237
Norberto Ruiz <narj@norbertoruiz.com>
2022-09-06 23:19:23 -0500

    Retornar valor en funcion

--- 3-funciiones/funciones.py
+++ 3-funciiones/funciones.py
@@ -6,19 +6,27 @@
 # imprimir_mensaje()
 # imprimir_mensaje()
 
-def conversacion(mensaje):
-    print('Hola')
-    print('Cómo estás')
-    print(mensaje)
-    print('Adios')
+# def conversacion(mensaje):
+#     print('Hola')
+#     print('Cómo estás')
+#     print(mensaje)
+#     print('Adios')
 
-opcion = int(input('Elige una opción (1, 2, 3): '))
-msg = 'Elegiste la opción '
-if opcion == 1:
-    conversacion(msg + str(opcion))
-elif opcion == 2:
-    conversacion(msg + str(opcion))
-elif opcion == 3:
-    conversacion(msg + str(opcion))
-else:
-    print('Escribe la opción correcta')
+# opcion = int(input('Elige una opción (1, 2, 3): '))
+# msg = 'Elegiste la opción '
+# if opcion == 1:
+#     conversacion(msg + str(opcion))
+# elif opcion == 2:
+#     conversacion(msg + str(opcion))
+# elif opcion == 3:
+#     conversacion(msg + str(opcion))
+# else:
+#     print('Escribe la opción correcta')
+
+def suma(a, b):
+    print('Se suman dos números')
+    resultado = a + b
+    return resultado
+
+sumatoria =  suma(1, 4)
+print(sumatoria)
